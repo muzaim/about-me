e1ca54c1f88a920c79a48e90af28100d6b05232b
muzaim <muzaim.surya@yahoo.co.id>
2023-07-25 10:50:22 +0700

    naik

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -24,20 +24,6 @@ const Header = () => {
 						<ul className="nav__list grid">
 							<li className="nav__item">
 								<a
-									href="#home"
-									onClick={() => setActiveNav("#home")}
-									className={
-										activeNav === "#home"
-											? "nav__link active-link"
-											: "nav__link"
-									}
-								>
-									<i className="uil uil-estate nav__icon"></i>{" "}
-									Home
-								</a>
-							</li>
-							<li className="nav__item">
-								<a
 									href="#about"
 									onClick={() => setActiveNav("#about")}
 									className={
--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -55,7 +55,6 @@ const Backend = () => {
 					</div>
 				</div>
 			</div>
-			``
 		</div>
 	);
 };
