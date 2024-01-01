e3d796faf702be2ed4916aa94ac7263ee773dcb9
IT Developer <it.developer@smf-indonesia.co.id>
2024-01-01 17:19:26 +0700

    done

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 				<Contact />
 			</main>
 			<Footer />
-			{/* <ScrollUp /> */}
+			<ScrollUp />
 		</>
 	);
 }
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -9,7 +9,7 @@ const Frontend = () => {
 					<div className="skills__data">
 						<i className="bx bx-badge-check"></i>
 						<div>
-							<h3 className="skills__name">Vue JS</h3>
+							<h3 className="skills__name">VueJS</h3>
 							<span className="skills__level">Intermediate</span>
 						</div>
 					</div>
