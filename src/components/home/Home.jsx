c6021c3e7c8d850bedc986a99ddea069f756350b
IT Developer <it.developer@smf-indonesia.co.id>
2024-01-01 12:41:33 +0700

    dsa

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,18 +5,18 @@ import Data from "./Data";
 import ScrollDown from "./ScrollDown";
 
 const Home = () => {
-  return (
-    <section className="home section" id="home">
-      <div className="home__container container grid">
-        <div className="home__content grid">
-          <Social />
-          <div className="home__img"></div>
-          <Data />
-        </div>
-        <ScrollDown />
-      </div>
-    </section>
-  );
+	return (
+		<section className="home section" id="home">
+			<div className="home__container grid">
+				<div className="home__content grid">
+					<Social />
+					<div className="home__img"></div>
+					<Data />
+				</div>
+				<ScrollDown />
+			</div>
+		</section>
+	);
 };
 
 export default Home;
